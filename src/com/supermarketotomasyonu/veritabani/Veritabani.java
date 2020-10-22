62ff5162f720f4b4a2df5e58df76a4c655d53b41
agitrubard <demiragitrubar@gmail.com>
2020-10-22 19:45:36 +0300

    Supermarket Otomasyonu

---
+++ src/com/supermarketotomasyonu/veritabani/Veritabani.java
@@ -0,0 +1,14 @@
+package com.supermarketotomasyonu.veritabani;
+
+/**
+ * @author berrinkorkutata
+ * @author agitrubardemir
+ */
+public class Veritabani {
+
+    public static final String username = "admin";
+    public static final String password = "tM4DYoNrBdIRyyQ0";
+    public static final String name = "SuperMarketOtomasyonu";
+    public static final String host = "localhost";
+    public static final int port = 8889;
+}
